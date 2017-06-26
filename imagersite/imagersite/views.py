fa386bb60d3dc286917b74a683e0ea4125646c21
Kurt <kurtrm@gmail.com>
2017-06-26 14:29:46 -0700

    Made some other changes, hopefully it works this time.

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,9 +1,5 @@
 """Project-level views."""
-from django.shortcuts import render, redirect
-from django.template import RequestContext
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
 from imager_images.models import Photo, Album
 import random
 
@@ -12,10 +8,12 @@ def home_view(request):
     """View for the home page."""
     photos = Photo.objects.all()
     images = [photos[i].photo.url for i, _ in enumerate(photos)]
+
     return render(
         request,
         'imagersite/home.html',
-        context={'content': 'cake', 'image': random.choice(images)}
+        context={'content': 'cake',
+                 'image': 'Random Image' if not images else random.choice(images)}
     )
 
 
