a723f7c4d383a7e8e01d732ac47aa38ad4b92d7f
Morgan Nomura <morganelle@gmail.com>
2017-06-26 18:03:00 -0700

    updated imagerprofile model

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from imager_images.models import Photo, Album
+from imager_profile.models import ImagerProfile
 
 
 @login_required
@@ -14,3 +15,14 @@ def profile_view(request):
         'imager_profile/profile.html',
         context={'photos': photos, 'albums': albums}
     )
+
+
+def public_profile(request, username):
+    """Display the public profile for a given username."""
+    # user_profile = ImagerProfile.objects.filter(user=username).all()
+    # return render(
+    #     request,
+    #     'imager_profile/public_profile.html',
+    #     context={'user': user_profile}
+    # )
+    pass
