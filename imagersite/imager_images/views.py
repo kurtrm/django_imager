39862fad6377fb3c0c064df6c13029931f523ae2
Kurt <kurtrm@gmail.com>
2017-06-27 18:02:34 -0700

    Built out views, urls, and templates, but we need to overcome some query issues to get the photos for each album displaying on the page.

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -2,8 +2,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Photo, Album
-from django.contrib.auth.models import User
-from imager_profile.models import ImagerProfile
 
 
 @login_required
@@ -15,4 +13,46 @@ def library(request):
         request,
         'imager_images/library.html',
         context={'photos': photos, 'albums': albums}
+    )
+
+
+def photos_view(request):
+    """View for multiple photos only."""
+    photos_pub = Photo.objects.filter(published='PB')
+    return render(
+        request,
+        'imager_images/photos.html',
+        context={'photos': photos_pub}
+    )
+
+
+def single_photo_view(request, photo_id):
+    """View for a single photo."""
+    #  Template for checking if public or private
+    photo = Photo.objects.get(id=photo_id)
+    return render(
+        request,
+        'imager_images/photo.html',
+        context={'photo': photo}
+    )
+
+
+def albums_view(request):
+    """View for multiple albums only."""
+    albums_pub = Album.objects.filter(published='PB')
+    return render(
+        request,
+        'imager_images/albums.html',
+        context={'albums': albums_pub}
+    )
+
+
+def single_album_view(request, album_id):
+    """View for a single album."""
+    album = Album.objects.get(id=album_id)
+    album_photos = [album.photos[i] for i in range(album.photos)]
+    return render(
+        request,
+        'imager_images/album.html',
+        context={'album': album_photos}
     )
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -9,14 +9,30 @@ from imager_profile.models import ImagerProfile
 @login_required
 def profile_view(request):
     """View for the profile page."""
-    photos_priv = Photo.objects.filter(user=request.user).filter(published='PV').count()
-    photos_pub = Photo.objects.filter(user=request.user).filter(published='PB').count()
-    albums_priv = Album.objects.filter(user=request.user).filter(published='PV').count()
-    albums_pub = Album.objects.filter(user=request.user).filter(published='PB').count()
+    photos_priv = (Photo.objects
+                   .filter(user=request.user)
+                   .filter(published='PV')
+                   .count())
+    photos_pub = (Photo.objects
+                  .filter(user=request.user)
+                  .filter(published='PB')
+                  .count())
+    albums_priv = (Album.objects
+                   .filter(user=request.user)
+                   .filter(published='PV')
+                   .count())
+    albums_pub = (Album.objects
+                  .filter(user=request.user)
+                  .filter(published='PB')
+                  .count())
     return render(
         request,
         'imager_profile/profile.html',
-        context={'photos_pub': photos_pub, 'photos_priv': photos_priv, 'albums_pub': albums_pub, 'albums_priv': albums_priv}
+        context={
+            'photos_pub': photos_pub,
+            'photos_priv': photos_priv,
+            'albums_pub': albums_pub,
+            'albums_priv': albums_priv}
     )
 
 
