91f91b195d8b70a2ffe31f3a7dcf03a3b2e9a5be
Morgan Nomura <morganelle@gmail.com>
2017-07-03 14:37:35 -0700

    updated public profile

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -41,8 +41,21 @@ def public_profile(request, request_username):
     request_username = request_username.lower()
     request_user = User.objects.filter(username=request_username)
     imager_profile = ImagerProfile.objects.filter(user=request_user)
+    photos_pub = (Photo.objects
+                  .filter(user=request_user)
+                  .filter(published='PB')
+                  )
+    albums_pub = (Album.objects
+                  .filter(user=request_user)
+                  .filter(published='PB')
+                  )
     return render(
         request,
         'imager_profile/public_profile.html',
-        context={'imager_user': imager_profile, 'username': request_username}
+        context={
+            'imager_user': imager_profile,
+            'username': request_username,
+            'photos_pub': photos_pub,
+            'albums_pub': albums_pub,
+        }
     )
