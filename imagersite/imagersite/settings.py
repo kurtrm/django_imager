bde829f764139e151b965a8d577f46d47b699001
Kurt <kurtrm@gmail.com>
2017-07-14 15:51:13 -0700

    Changed a setting in preparation for deployment.

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'bug')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(eval(os.environ.get('DEBUG', 'False')))
 
-ALLOWED_HOSTS = ['http://ec2-13-59-234-227.us-east-2.compute.amazonaws.com/', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['ec2-13-59-234-227.us-east-2.compute.amazonaws.com', '127.0.0.1', 'localhost']
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
